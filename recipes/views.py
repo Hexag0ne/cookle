58846e8ca04eaf8f4e9000fdbda5a983166d527e
Julia <juliettebricout@live.fr>
2016-11-07 09:14:51 +0100

    Fix templates related issues

--- recipes/views.py
+++ recipes/views.py
@@ -6,5 +6,9 @@ import json
 def index():
 	return render_template('index.html')
 
+@app.route('/results')
+def results():
+	return render_template('results.html')
+
 if __name__ == '__main__':
 	app.run()
