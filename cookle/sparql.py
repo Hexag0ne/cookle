11ec8edf2cf776a53380b4bd71bc8dd7a6c8907e
patrick <patrick.finnerty@insa-lyon.fr>
2016-11-09 09:33:30 +0100

    Créé une première requète SPARQL qui récupère la description anglaise d'un uri

--- cookle/sparql.py
+++ cookle/sparql.py
@@ -1,13 +1,28 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-sparql.setQuery("""
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT ?label
-    WHERE { <http://dbpedia.org/resource/Asturias> rdfs:label ?label }
-""")
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
+#sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+#sparql.setQuery("""
+#    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+#    SELECT ?label
+#    WHERE { <http://dbpedia.org/resource/Asturias> rdfs:label ?label }
+#""")
+#sparql.setReturnFormat(JSON)
+#results = sparql.query().convert()
+#
+#for result in results["results"]["bindings"]:
+#    print(result["label"]["value"])
 
-for result in results["results"]["bindings"]:
-    print(result["label"]["value"])
+
+spar = SPARQLWrapper("http://dbpedia.org/sparql")
+spar.setQuery("""
+	SELECT ?comment WHERE {
+	<http://dbpedia.org/resource/Tajine> rdfs:comment ?comment
+	FILTER (lang(?comment) = 'en')
+
+}
+""")
+spar.setReturnFormat(JSON)
+resul = spar.query().convert()
+
+for resultat in resul["results"]["bindings"]:
+	print(resultat["comment"]["value"])
