f10cf3a22f43bd8ac1660fa0139613a2eb6f90e5
patrick <patrick.finnerty@insa-lyon.fr>
2016-11-09 11:31:44 +0100

    Mis en place la récupération paramétrée de la description d'un URI

--- cookle/sparql.py
+++ cookle/sparql.py
@@ -1,31 +1,23 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-#sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-#sparql.setQuery("""
-#    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-#    SELECT ?label
-#    WHERE { <http://dbpedia.org/resource/Asturias> rdfs:label ?label }
-#""")
-#sparql.setReturnFormat(JSON)
-#results = sparql.query().convert()
-#
-#for result in results["results"]["bindings"]:
-#    print(result["label"]["value"])
 
-def getDescriptionAnglais(uri):
+def test():
+	print(getDescription("<http://dbpedia.org/resource/Tajine>","en"))
+	print(getDescription("<http://dbpedia.org/resource/Tajine>","fr"))
+
+
+
+def getDescription(uri,langue):
+	"""Permet de récupérer la description d'une ressource dbpedia dans la langue souhaitée """
 	spar = SPARQLWrapper("http://dbpedia.org/sparql")
 	spar.setQuery("""
 	SELECT ?comment WHERE { """ + uri + 
 	"""	rdfs:comment ?comment
-	FILTER (lang(?comment) = 'en')
+	FILTER (lang(?comment) = '""" + langue + """')
 	} """)
 	spar.setReturnFormat(JSON)
 	resul = spar.query().convert()
 
-#	for resultat in resul["results"]["bindings"]:
-#		print(resultat["comment"]["value"])
-	print(resul["results"]["bindings"][0]["comment"]["value"])
-	return resul["results"]["bindings"][0]["comment"][value"]
+	return resul["results"]["bindings"][0]["comment"]["value"]
 
 
-fic["description_en"] = getDescriptionAnglais(uri)
