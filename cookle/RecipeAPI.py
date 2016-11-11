46a5b414120ca183414203f230e978e073f6de2a
yMorakkam <yassine.kam@gmail.com>
2016-11-11 22:49:58 +0100

    json ajouté

--- cookle/RecipeAPI.py
+++ cookle/RecipeAPI.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import requests
 import json
 import spotlight 
@@ -7,9 +8,12 @@ list_ingredients = []
 """Liste des Uris (ressources) des ingrédients obtenus à partir de l'api DBPedia Spotlight""" 
 list_Uri = []
 """ A récuperer / input clavier"""
-motcle =''
+motcle ='cake'
 """ url de l'api pour faire la recherche, on reçoit un objet JSON """
-requette="https://api.edamam.com/search?q={0}&app_id=0806efd4&app_key=13a926d1babeb8a3726cc3eead90e57c&from=0&to=3".format(motcle)
+""" fichier de retour JSON """
+recette_json={} 
+
+requette="https://api.edamam.com/search?q={0}&app_id=0806efd4&app_key=13a926d1babeb8a3726cc3eead90e57c&from=0&to=3".format('cake')
 resultat = requests.get(requette)
 resultat = resultat.json()
 ingredients = resultat['hits'][0]['recipe']['ingredients']
@@ -28,7 +32,7 @@ for ingredient in ingredients:
 	
 		list_ingredients.append([nom_ingredient,quantite_ingredient,unite_ingredient])
 	
-		annotation = spotlight.annotate('http://spotlight.sztaki.hu:2222/rest/annotate',nom_ingredient,confidence=0.1, support=20)
+		annotation = spotlight.annotate('http://spotlight.sztaki.hu:2222/rest/annotate',text=nom_ingredient,confidence=0.1, support=20,spotter='Default')
 		uri_ingredient = annotation[0]['URI']
 		
 	except spotlight.SpotlightException:
@@ -37,6 +41,10 @@ for ingredient in ingredients:
 	list_Uri.append(uri_ingredient)
 
 """ Uri de la recette récupéré grâce à l'api DBPedia Spotlight """
-annotation = spotlight.annotate('http://spotlight.sztaki.hu:2222/rest/annotate',motcle,confidence=0.1, support=20)
+annotation = spotlight.annotate('http://spotlight.sztaki.hu:2222/rest/annotate',text=motcle,confidence=0.1, support=20,spotter='Default')
 uri_recette = annotation[0]['URI']
+
+recette_json["ingredients"]=list_ingredients
+recette_json=json.dumps(recette_json, indent=4)
+
 		
