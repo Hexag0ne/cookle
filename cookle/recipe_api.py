1547c00b475708141611079b6fcda11ce1b7f9f8
Mehdi BAHA <elmehdi.baha@gmail.com>
2016-11-16 23:32:49 +0100

    Updated search engine

--- cookle/recipe_api.py
+++ cookle/recipe_api.py
@@ -3,46 +3,52 @@ import requests
 import json
 import spotlight
 import goslate
-from sparql import getNomFrancais
+from sparql import getNomFrancais, getImage
 
 """Liste des ingredients. Chaque ingrédient est défini par une liste composée de son nom,de sa proportion et de l'unité de mesure. Donc c'est une liste de listes. """
-list_ingredients = []
 """Liste des Uris (ressources) des ingrédients obtenus à partir de l'api DBPedia Spotlight""" 
-list_Uri = []
 """ A récuperer / input clavier"""
-motcle ='cake'
 """ url de l'api pour faire la recherche, on reçoit un objet JSON """
 """ fichier de retour JSON """
 
 def getIngredients(recipe):
-    gs = goslate.Goslate()
-    recipe_tr = gs.translate( recipe, 'en')
-    requette="https://api.edamam.com/search?q={0}&app_id=0806efd4&app_key=13a926d1babeb8a3726cc3eead90e57c&from=0&to=3&diet=balanced".format(recipe_tr)
+    list_ingredients = []
+    gs = goslate.Goslate(service_urls=['http://translate.google.de'])
+    recipe_tr = translate(recipe)
+    requette = "https://api.edamam.com/search?q={0}&app_id=0806efd4&app_key=13a926d1babeb8a3726cc3eead90e57c&from=0&to=3&diet=balanced".format(recipe_tr)
     resultat = requests.get(requette)
     resultat = resultat.json()
+    if len(resultat['hits'])==0:
+        return dict(list_ingredients=[], uri=None)
     ingredients = resultat['hits'][0]['recipe']['ingredients']
+    i = 1
     for ingredient in ingredients:
+        nom_ingredient = ingredient['food']
+        if ingredient['measure']=='lb' : 
+            quantite_ingredient = ingredient['weight'] 
+            unite_ingredient = 'g'
+        else: 
+            quantite_ingredient= ingredient['quantity']
+            if float(quantite_ingredient) % 1 == 0:
+                quantite_ingredient = int(float(quantite_ingredient))
+            else:
+                quantite_ingredient = round(float(quantite_ingredient), 2)
+            unite_ingredient = ingredient['measure']
         try:
-            nom_ingredient = ingredient['food']
-            if ingredient['measure']=='lb' : 
-                quantite_ingredient = ingredient['weight'] 
-                unite_ingredient = 'g'
-            else: 
-                quantite_ingredient= ingredient['quantity']
-                if float(quantite_ingredient) % 1 == 0:
-                    quantite_ingredient = int(float(quantite_ingredient))
-                else:
-                    quantite_ingredient = float(quantite_ingredient)
-                unite_ingredient = ingredient['measure']
             annotation = spotlight.annotate('http://spotlight.sztaki.hu:2222/rest/annotate',text=nom_ingredient,confidence=0.1, support=20,spotter='Default')
             uri_ingredient = annotation[0]['URI']
+            image_url = getImage(uri_ingredient)
             try:
                 nom_ingredient = getNomFrancais(uri_ingredient)
             except:
-                nom_ingredient = nom_ingredient + " (anglais)"
+                nom_ingredient = "{} (anglais)".format(nom_ingredient)
         except spotlight.SpotlightException:
             uri_ingredient = None
-        list_ingredients.append(dict(name=nom_ingredient, quantity=quantite_ingredient, unit=unite_ingredient, uri=uri_ingredient))
+        if nom_ingredient:
+            i += 1
+            list_ingredients.append(dict(name=nom_ingredient, quantity=quantite_ingredient, unit=unite_ingredient, uri=uri_ingredient, image_url=image_url))
+        if i > 5:
+            break
 
     """ Uri de la recette récupéré grâce à l'api DBPedia Spotlight """
     try:
@@ -54,3 +60,7 @@ def getIngredients(recipe):
     # uri can be None
     recette = dict(list_ingredients=list_ingredients, uri=uri_recette)
     return recette
+
+def translate(word):
+    req = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20161116T102126Z.efd63f26cc4533d2.7e0ab34d8ae9647e547a4b450788b5de86a9e6a6&text={}&lang=fr-en".format(word)
+    return requests.get(req).json()['text'][0]
